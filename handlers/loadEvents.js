2bc9807004ed8e55af61fa10342d5767861d3fbf
Axteli <70712278+Axteli@users.noreply.github.com>
2022-02-22 09:21:34 +0100

    Create loadEvents.js

---
+++ handlers/loadEvents.js
@@ -0,0 +1,12 @@
+const Discord = require("discord.js");
+const eventClient = (event) => require(`../events/client/${event}`);
+
+function loadEvents(client) {
+
+    //client event
+    client.on("ready", () => eventClient("ready.js")(client));
+
+};
+module.exports = {
+    loadEvents
+};
