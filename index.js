98ef0662d91dec065c1531875dc2fa3b4377df94
Axteli <70712278+Axteli@users.noreply.github.com>
2022-02-22 08:28:20 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+const Discord = require("discord.js");
+const intents = Discord.Intents;
+const client = new Discord.Client({intents: [intents.FLAGS.GUILDS, intents.FLAGS.GUILD_MESSAGES]});
+const config = require("./config.json");
+
+const loadCommands = require("./handlers/loadCommands.js");
+const loadEvents = require("./handlers/loadEvents.js");
+
+
+//create all collections for commands
+client.commands = new Discord.Collection();
+client.description = new Discord.Collection();
+client.usage = new Discord.Collection();
+client.example = new Discord.Collection();
+client.aliases = new Discord.Collection();
+client.categories = fs.readdirSync('./commands/');
+
+
+//execute handlers
+loadCommands(client);
+loadEvents(client);
+
+
+//login
+client.login(config.token);
