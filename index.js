f72e078d379ef49b14573a0648fd2f2a2e064ca3
Axteli <70712278+Axteli@users.noreply.github.com>
2022-03-14 12:34:15 +0100

    Remove loadCommands from index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const DiscordBot = require("./helpers/Client.js")
 const client = new DiscordBot();
 require("dotenv").config();
-const { loadCommands } = require("./handlers/loadCommands.js");
 const { loadEvents } = require("./handlers/loadEvents.js");
 
 
