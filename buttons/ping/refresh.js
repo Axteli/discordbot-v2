2412c1f2cac9348e1930ba4124f20a59d98af44d
Axteli <70712278+Axteli@users.noreply.github.com>
2022-03-14 12:25:44 +0100

    Add button to ping command

---
+++ buttons/ping/refresh.js
@@ -0,0 +1,24 @@
+const { MessageEmbed } = require("discord.js");
+module.exports = {
+	name: "ping/refresh",
+	async run(client, interaction) {
+
+
+		const start = Date.now();
+
+		await interaction.deferReply({ ephemeral: true });
+
+		const end = Date.now();
+
+
+		const embed = new MessageEmbed()
+			.setColor(process.env.EMBED_COLOR)
+			.setTitle("Pong 🏓")
+			.setDescription(`**Api's ping** : ${client.ws.ping} ms\n**Bot's ping** : ${end - start} ms`);
+
+
+		interaction.editReply({ embeds: [embed] });
+
+
+	}
+}
--- commands/info/ping.js
+++ commands/info/ping.js
@@ -1,4 +1,4 @@
-const { MessageEmbed } = require("discord.js")
+const { MessageEmbed, MessageActionRow, MessageButton } = require("discord.js");
 
 module.exports = {
 	name: "ping",
@@ -7,22 +7,29 @@ module.exports = {
 	userPermissions: [],
 	async run(client, interaction) {
 
+
 		const start = Date.now();
 
-		const firstEmbed = new MessageEmbed()
-			.setColor(process.env.EMBED_COLOR)
-			.setTitle("Bot ping")
-			.setDescription("Calculate bot's ping...")
-
-		await interaction.reply({ embeds: [firstEmbed] })
+		await interaction.deferReply();
 
 		const end = Date.now();
 
-		const secondEmbed = new MessageEmbed()
+
+		const embed = new MessageEmbed()
 			.setColor(process.env.EMBED_COLOR)
 			.setTitle("Pong 🏓")
-			.setDescription(`**Api ping** : ${client.ws.ping} ms\n**Bot's ping** : ${end - start} ms`)
+			.setDescription(`**Api ping** : ${client.ws.ping} ms\n**Bot's ping** : ${end - start} ms`);
 
-		interaction.editReply({ embeds: [secondEmbed] })
+
+		const buttons = new MessageActionRow()
+			.addComponents(
+				new MessageButton()
+					.setStyle("SUCCESS")
+					.setCustomId("ping/refresh")
+					.setLabel("Refresh")
+			);
+
+
+		interaction.editReply({ embeds: [embed], components: [buttons] });
 	}
-}
+};
