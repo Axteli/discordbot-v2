e162097462c92eb6bf7ca5718622212e58510da8
Axteli <70712278+Axteli@users.noreply.github.com>
2022-03-28 13:28:55 +0200

    whoops

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const DiscordBot = require("./helpers/Client.js")
 const client = new DiscordBot();
 require("dotenv").config();
 const { loadEvents } = require("./handlers/loadEvents.js");
+const { loadButtons } = require("./handlers/loadButtons.js")
 
 
 //load events
